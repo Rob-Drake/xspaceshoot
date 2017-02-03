792b03640e0d4c91a313b3852c6cdfe25b84fe27
Rob-Drake <draker1001@yahoo.com>
2017-02-03 00:19:15 -0600

    updated

--- js/player.js
+++ js/player.js
@@ -1,11 +1,19 @@
 var xSpaceShoot = xSpaceShoot || {};
 
-xSpaceShoot.Player = function(game, key) {
+xSpaceShoot.Player = function(game, key, weap) {
 	Phaser.Sprite.call(this. game, 0, 0, key);
 	this.texture.baseTexture.scaleMode = PIXI.scaleModes.NEAREST;
 	this.anchor.set(0.5, 0.5);
+	this.weapon = weap;
 };
 
 xSpaceShoot.Player.prototype = Object.create(Phaser.Sprite.prototype);
 xSpaceShoot.Player.prototype.constructor = xSpaceShoot.Player;
 
+xSpaceShoot.Player.prototype.setWeapon = function(weap) {
+	this.weapon = weap;
+};
+
+xSpaceShoot.Player.prototype.fire = function() {
+	this.weapon.fire(); 	//look up parameters needed
+};
